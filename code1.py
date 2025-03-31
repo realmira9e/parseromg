af21f2fcf820af5c6013486802b086eb9b313d83
mirage <56506813+realmira9e@users.noreply.github.com>
2025-03-31 12:21:49 +0300

    Update code1.py

--- code1.py
+++ code1.py
@@ -1,6 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
+def get_image_links(article):
+    image_tag = article.find('img')
+    if image_tag and 'src' in image_tag.attrs:
+        return "https:" + image_tag['src']
+    return "Нет изображения"
+
 search_query = 'Банан'
 base_url = 'https://ru.wikipedia.org'
 search_url = f'{base_url}/w/index.php?search={search_query.replace(" ", "+")}&title=Служебная:Поиск&profile=advanced&fulltext=1&ns0=1'
@@ -22,10 +28,14 @@ for article in article_items[:5]:
     description_tag = article.find('div', class_='searchresult')
     description = description_tag.text.strip()  if description_tag else "Нет описания"
 
+    image_link = get_image_links(article)
+
     print(
         f'''
 Название: {title}
 Вес, кол-во слов и последняя дата изменения: {data}
 Описание: {description}
+
+Ссылка на изображение: {image_link}
 '''
     )
